4f6cd51790737335ad34c5eaa83f8dbaedb46ede
David Kunwar <dkunwar7111@gmail.com>
2023-01-23 22:51:18 +0530

    commit from mac

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res){
     res.send("Hello World")
 })
 
+app.get('/mac', function(req, res){
+    res.send("Hello from macbook")
+})
+
 app.listen(3000, function(req, res){
     console.log("Server is up and running at PORT : 3000")
 })
